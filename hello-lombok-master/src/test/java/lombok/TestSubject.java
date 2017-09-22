a8a6c38877f8b6b462b4a69644817d51dcff609f
unknown <312636208@qq.com>
2017-09-22 12:29:53 +0800

    add_test

---
+++ hello-lombok-master/src/test/java/lombok/TestSubject.java
@@ -0,0 +1,17 @@
+package lombok;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA for hello-lombok.
+ * User: EremenkoAA
+ * Date: 21.10.13
+ */
+@HelloWorld
+public class TestSubject {
+
+//    public void helloWorld() {
+//        System.out.println("Hello World");
+//    }
+
+}
